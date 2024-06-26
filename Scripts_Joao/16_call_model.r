897141ec03fa0ad1a4e6632a4364b5db58986e83
João Victor Lenzi Cardoso <97787543+KouranliKe@users.noreply.github.com>
2024-06-26 13:27:32 -0400

    Add files via upload
    unifying functions in a single file

---
+++ Scripts_Joao/16_call_model.r
@@ -0,0 +1,56 @@
+rm(list = ls())
+library(fbi)
+library(tidyverse)
+library(TTR)
+
+source("13_data_prep.r")
+source("functions.r")
+source("15_rolling_window.r")
+
+#####
+## The file with the forecasts will be saved with model_name
+model_name <- "lasso"
+## The function called to run models is model_function, which is a function from functions.R
+model_function <- get_lasso
+#####
+
+load("data_joao.rda")
+dates <- data$date
+data <- data %>%
+  select(-date) %>%
+  as.matrix()
+rownames(data) <- as.character(dates)
+
+####### run rolling window ##########
+
+nwindows <- 52 # num. de janelas ou num. de previsões
+y_out <- tail(data[, "CPIAUCSL"], nwindows)
+model_list <- list()
+for_ind <- c(1, 3, 6, 12) # 1:12
+
+for (i in for_ind) {
+  model <- rolling_window(
+    fn = model_function,
+    df = data,
+    nwindow = nwindows + i - 1,
+    horizon = i,
+    variable = "CPIAUCSL",
+    n_lags = 4
+  )
+  model_list[[i]] <- model
+  cat(i, "\n")
+}
+
+forecasts <- Reduce(
+  f = cbind,
+  x = lapply(model_list, function(x) head(x$forecast, nwindows))
+) %>% as.matrix()
+
+plot.ts(y_out)
+lines(forecasts[, 1], col = 2)
+
+f_rmse <- function(x, y) {
+  sqrt(mean((x - y)^2))
+}
+
+rmse <- apply(forecasts, 2, f_rmse, y = y_out) %>% print()
